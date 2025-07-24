8c76519055d3ed727ac91a0572c86cf7114ce736
Piotr <grabowskipiotr1989@gmail.com>
2025-07-24 07:01:46 +0200

    new config

--- src/app/firebase/firebase.config.ts
+++ src/app/firebase/firebase.config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDDQipdWDnBshdv19BfhZ9xt04cH1fX_8o',
-  authDomain: 'boardgame-99792.firebaseapp.com',
-  projectId: 'boardgame-99792',
-  storageBucket: 'boardgame-99792.appspot.com',
-  messagingSenderId: '164029620212',
-  appId: '1:164029620212:web:3f6ebb543774c37a74a3a1',
-  measurementId: 'G-H56Y2QXDYS',
+  apiKey: 'AIzaSyAK5Q9RCM4vnadEyP7bRllMHqAOWqRMTF8',
+  authDomain: 'boardgame-app-5422c.firebaseapp.com',
+  projectId: 'boardgame-app-5422c',
+  storageBucket: 'boardgame-app-5422c.firebasestorage.app',
+  messagingSenderId: '55541457543',
+  appId: '1:55541457543:web:3aeff1aa7b8e4e4634ce22',
 };
